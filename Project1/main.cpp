333dc9b988b45adef4eaec11047e091dc2e506ac
syki1 <syki10@gmail.com>
2020-11-10 08:09:41 +0100

    Added includes and user's choice

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
 
+/* Blob storage */
 #include <was/storage_account.h>
 #include <was/blob.h>
 #include <cpprest/filestream.h>  
 #include <cpprest/containerstream.h> 
 
+/* Table storage */
+#include <was/storage_account.h>
+#include <was/table.h>
+
 using namespace std;
 
 int main(int argc, char **argv)
 {
-	
+	if (argc < 2)
+		cout << "Give more parameters" << endl;
+
+	int choice = 0;
+
+	switch (choice)
+	{
+		case 0:
+			break;
+		case 1:
+			break;
+		case 2:
+		default:
+			cout << "Unknown option" << endl;
+	}
 		
 
 
