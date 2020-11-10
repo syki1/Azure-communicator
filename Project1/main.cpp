7d232ead6c204b93b2bded6752f8a3ee5da35c54
syki1 <syki10@gmail.com>
2020-11-10 10:29:34 +0100

    Added delete functionality

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char** argv)
 		cout << "argv[i] == " << argv[i] << endl;
 	}
 
-	if (argc > 1)
+	if (argc >= 2)
 	{
 		string user = "qazsed91@gmail.com";
 		string password = "haslo123";
@@ -67,42 +67,44 @@ int main(int argc, char** argv)
 			// Create or overwrite the "my-blob-2" and "my-blob-3" blobs with contents from text.
 			// Retrieve a reference to a blob named "my-blob-2".
 			azure::storage::cloud_block_blob blob2 = container.get_block_blob_reference(U("my-blob-2"));
-			blob2.upload_text(U("more text"));
+			blob2.upload_from_file(U("file.txt"));
 
 			/* // Retrieve a reference to a blob named "my-blob-3".
 			azure::storage::cloud_block_blob blob3 = container.get_block_blob_reference(U("my-directory/my-sub-directory/my-blob-3"));
 			blob3.upload_text(U("other text")); */
 
-
-
-			choice = ADD;
 		}
 		else if (command == "--delete")
 		{
-			choice = DEL;
+			cout << "Deleting file.txt from Azure service ";
+			// Define the connection-string with your values.
+			const utility::string_t storage_connection_string(U("DefaultEndpointsProtocol=https;AccountName=qazsed91@gmail.com;AccountKey=haslo123"));
+
+			// Retrieve storage account from connection string.
+			azure::storage::cloud_storage_account storage_account = azure::storage::cloud_storage_account::parse(storage_connection_string);
+
+			// Create the blob client.
+			azure::storage::cloud_blob_client blob_client = storage_account.create_cloud_blob_client();
+
+			// Retrieve a reference to a previously created container.
+			azure::storage::cloud_blob_container container = blob_client.get_container_reference(U("filesstorage2"));
 			
+			// Retrieve reference to a blob named "my-blob-1".
+			azure::storage::cloud_block_blob blockBlob = container.get_block_blob_reference(U("files"));
+
+			// Delete the blob.
+			blockBlob.delete_blob();
 		}
 			
 		else if (command == "--logs")
-			choice = LOG;
+		{
+			cout << "logs" << endl;
+		}
 		else
 		{
-			choice = UNKNOWN;
 			cout << "Unknown option";
 		}
 			
-
-		switch (choice)
-		{
-		case ADD:
-			break;
-		case DEL:
-			break;
-		case LOG:
-			break;
-		default:
-			cout << "Unknown option" << endl;
-		}
 	}
 	else
 	{
