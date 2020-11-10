db8e37f3d57cf02de3e2c8d7a74e930003c79e9f
syki1 <syki10@gmail.com>
2020-11-10 07:56:40 +0100

    Created c++ project

---
+++ Project1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include <was/storage_account.h>
+#include <was/blob.h>
+#include <cpprest/filestream.h>  
+#include <cpprest/containerstream.h> 
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	
+		
+
+
+	return 0;
+}
